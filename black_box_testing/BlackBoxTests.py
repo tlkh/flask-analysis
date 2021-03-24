c18072516f731b616c0a8381cb571a5dac0df36a
Timothy Liu <tlkh.xms@gmail.com>
2021-03-24 20:14:29 +0800

    Add unit tests

---
+++ black_box_testing/BlackBoxTests.py
@@ -0,0 +1,148 @@
+import time
+import unittest
+import requests
+from seleniumwire import webdriver
+
+CHROMEDRIVER_PATH = "./chromedriver_mac"
+
+class TestServerRunning(unittest.TestCase):
+    """
+    Check if server is running by checking homepage
+    """
+    def setUp(self):
+        self.driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+    def test(self):
+        self.driver.get("http://localhost:5000")
+        hw_text = self.driver.find_element_by_id("helloworld").get_attribute("innerHTML")
+        self.assertEqual(hw_text, "Hello, World!")
+    def tearDown(self):
+        self.driver.quit()
+        
+class Test404Page(unittest.TestCase):
+    """
+    Check 404 page
+    """
+    def setUp(self):
+        self.driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+    def test(self):
+        self.driver.get("http://localhost:5000/invalid_page")
+        page_title = self.driver.title 
+        self.assertEqual(page_title, "404 Not Found")
+    def tearDown(self):
+        self.driver.quit()
+
+class TestEscapeHTMLInputs(unittest.TestCase):
+    """
+    Check that HTML in inputs are escaped
+    """
+    def setUp(self):
+        self.driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+    def test(self):
+        self.driver.get("http://localhost:5000/inputs")
+        search_box = self.driver.find_element_by_id("inputbox")
+        search_box.send_keys("<h1 id='unsafe_element'>Unsafe Element</h1>")
+        search_box.submit()
+        display_out = self.driver.find_element_by_id("displayout").get_attribute("innerHTML")
+        try:
+            unsafe_element = driver.find_element_by_id("unsafe_element").get_attribute("innerHTML")
+            unsafe_element_found = True
+        except:
+            unsafe_element_found = False
+        self.assertEqual(unsafe_element_found, False)
+    def tearDown(self):
+        self.driver.quit()
+        
+class TestEscapeCSSInputs(unittest.TestCase):
+    """
+    Check that CSS in inputs are escaped
+    """
+    def setUp(self):
+        self.driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+    def test(self):
+        self.driver.get("http://localhost:5000/inputs")
+        search_box = self.driver.find_element_by_id("inputbox")
+        search_box.send_keys("<style>body{background-color:red}</style>")
+    def tearDown(self):
+        self.driver.quit()
+        
+class TestEscapeJSInputs(unittest.TestCase):
+    """
+    Check that JS in inputs are escaped
+    """
+    def setUp(self):
+        self.driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+    def test(self):
+        pass
+    def tearDown(self):
+        self.driver.quit()
+        
+class TestSecurityHeaders(unittest.TestCase):
+    """
+    Check security headers on test page
+    """
+    def setUp(self):
+        pass
+    def test(self):
+        res_headers = requests.get("http://localhost:5000/test_headers").headers
+        correct_headers = {'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+                           'Content-Security-Policy': "default-src 'self'",
+                           'X-Content-Type-Options': 'nosniff',
+                           'X-Frame-Options': 'SAMEORIGIN',
+                           'X-XSS-Protection': '1; mode=block'}
+        for k in list(correct_headers.keys()):
+            self.assertEqual(res_headers[k], correct_headers[k])
+    def tearDown(self):
+        pass
+    
+class TestServerFingerprint(unittest.TestCase):
+    """
+    Check Flask server is able to hide fingerprint
+    """
+    def setUp(self):
+        pass
+    def test(self):
+        res_headers = requests.get("http://localhost:5000/test_headers").headers
+        self.assertEqual(res_headers["Server"], "None")
+    def tearDown(self):
+        pass
+    
+class TestCookieLifetime(unittest.TestCase):
+    """
+    Test cookie lifetime is properly set
+    """
+    def setUp(self):
+        self.driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+        self.driver.delete_all_cookies()
+    def test(self):
+        self.driver.get("http://localhost:5000/test_cookie_options")
+        initial_cookies = self.driver.get_cookies()
+        time.sleep(2)
+        self.driver.get("http://localhost:5000/test_cookie_options")
+        second_cookies = self.driver.get_cookies()
+        # we should have more cookies
+        self.assertGreater(len(second_cookies), len(initial_cookies))
+        # but some cookies from initial visit should expire
+        self.assertLess(len(second_cookies), len(initial_cookies)*2)
+    def tearDown(self):
+        self.driver.quit()
+        
+class TestDebuggerVisibility(unittest.TestCase):
+    """
+    Test debugger is not visible
+    """
+    def setUp(self):
+        self.driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+    def test(self):
+        self.driver.get("http://localhost:5000/test_exception")
+        try:
+            traceback = self.driver.find_elements_by_class_name("debugger")
+            if len(traceback) > 0:
+                traceback_found = True
+            else:
+                traceback_found = False
+        except:
+            traceback_found = False
+        self.assertEqual(traceback_found, False)
+    def tearDown(self):
+        self.driver.quit()
+        
---
+++ black_box_testing/server.py
@@ -0,0 +1,69 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--debug", action="store_true", default=False,
+                    help="Use debug mode to launch Flask server")
+parser.add_argument("--ssl", action="store_true", default=False,
+                    help="Use self-signed SSL with Flask server")
+parser.add_argument("--port", type=int, default=5000)
+args = parser.parse_args()
+
+import time
+from flask import Flask
+from flask import render_template, request, make_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html")
+
+@app.route("/inputs", methods=["GET", "POST"])
+def inputs_page(data="No data"):
+    if request.method == "POST":
+        data = request.form["text"]
+    return render_template("inputs.html", data=data)
+
+@app.route("/inputs_unsafe", methods=["GET", "POST"])
+def inputs_page_unsafe(data="No data"):
+    if request.method == "POST":
+        data = request.form["text"]
+    return render_template("inputs_unsafe.html", data=data)
+
+@app.route("/test_headers")
+def test_headers_page():
+    r = make_response(render_template("home.html"))
+    r.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+    r.headers["Content-Security-Policy"] = "default-src 'self'"
+    r.headers["X-Content-Type-Options"] = "nosniff"
+    r.headers["X-Frame-Options"] = "SAMEORIGIN"
+    r.headers["X-XSS-Protection"] = "1; mode=block"
+    r.headers["Server"] = "None"
+    return r
+
+@app.route("/test_cookie_options")
+def test_cookies_page():
+    app.config.update(
+        SESSION_COOKIE_SECURE=True,
+        SESSION_COOKIE_HTTPONLY=True,
+        SESSION_COOKIE_SAMESITE="Lax",
+        PERMANENT_SESSION_LIFETIME=600
+    )
+    r = make_response(render_template("home.html"))
+    cookie_name = str(int(time.time()))
+    #r.set_cookie("username", "flask", secure=True, httponly=True, samesite="Lax")
+    r.set_cookie(cookie_name, "1", max_age=1)
+    r.set_cookie(cookie_name+"_persistent", "2", max_age=100)
+    return r
+
+@app.route("/test_exception")
+def test_exception_page():
+    result = 1/0
+    return "Exception"
+
+if __name__ == "__main__":
+    print("Launching with adhoc SSL certificate:", args.ssl)
+    if args.ssl:
+        app.run(host="127.0.0.1", port=args.port, debug=args.debug, ssl_context="adhoc")
+    else:
+        app.run(host="127.0.0.1", port=args.port, debug=args.debug)
+    
